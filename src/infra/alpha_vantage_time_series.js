928c880b2c075d5cfca259b06b42f63e02dbfa8a
Gabri Franca <gvictor1596@gmail.com>
2021-01-02 21:11:52 -0300

    feat: return a aritimetic average of specified symbol

--- src/infra/alpha_vantage_time_series.js
+++ src/infra/alpha_vantage_time_series.js
@@ -3,10 +3,9 @@ require('dotenv').config();
 
 const getTimeSeriesDaily = async (symbol, outputsize) => {
     const url = 'https://www.alphavantage.co/query';
-    const requestfunction = 'TIME_SERIES_DAILY';
-    symbol = 'VVAR3.SA'
-    outputsize = 'full'
+    const requestfunction = 'TIME_SERIES_DAILY_ADJUSTED';
 
+    
     try {
      const data = await axios.get( url, {
                 params: {
@@ -16,13 +15,26 @@ const getTimeSeriesDaily = async (symbol, outputsize) => {
                     apikey: 'JXKL6Z5ZBERI886Z'
                 }
             } 
-        )
+        );
+        const rawSeries = data.data["Time Series (Daily)"];
 
-    return data.data;  
+        const series = Object.keys(rawSeries).reverse().map(date => {
+            return {date, 
+                open: parseFloat(rawSeries[date]['1. open']),
+                high: parseFloat(rawSeries[date]['2. high']),
+                low: parseFloat(rawSeries[date]['3. low']),
+                close: parseFloat(rawSeries[date]['4. close']),
+                adjustedClose: parseFloat(rawSeries[date]['5. adjusted close']),
+                volume: parseFloat(rawSeries[date]['6. volume']),
+                dividendAmount: parseFloat(rawSeries[date]['7. dividend amount']),
+                splitCoeficient: parseFloat(rawSeries[date]['8. split coefficient'])
+            }
+        });
+        return series;
 
     } catch (e) {
         console.log(e);
     }
 }
 
-getTimeSeriesDaily();
+module.exports = { getTimeSeriesDaily }
